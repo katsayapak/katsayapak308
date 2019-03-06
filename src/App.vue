550931096f0f6523b27c9888f03b0d6cffd806b5
katsayapak <123202601maymo>
2019-03-06 11:00:23 +0700

    final

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link class="a" to="/">แสดงข้อมูลทั้งหมด</router-link> 
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+.a {
+  background-color: #ffff00; /* Green */
+  border: none;
+  color: rgb(252, 237, 237);
+  padding: 15px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+.a {border-radius: 12px;}
+
 #app {
+  background-repeat: no-repeat;
+  background-size: auto;
+  background-color: #e0fdb0; 
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    
   </div>
 </template>
+ <script>
+export default {
+  data() {
+    return {
+      users: []
+    };
+  },
+  mounted() {
+    axios
+      .get("https://vue-js-1a60d.firebaseio.com/")
+      .then(response => {
+        console.log(response);
+        const data = response.data;
+        for (let key in data) {
+          const user = data[key];
+          user.id = key;
+          this.users.push(user);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+};
+</script>
+ 
